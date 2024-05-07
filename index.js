243b60f9cdf3cd54c6c2bb38e3e85adafc9c7edb
MathieuPar60 <mathieu-1.parenteau@polymtl.ca>
2024-05-07 11:43:38 -0400

    mp | portfolio starwars theme

---
+++ index.js
@@ -0,0 +1,12 @@
+/*const items = ["HTML", "CSS", "JavaScript", "Python", "C++", "React", "Angular"];
+
+const gridContainer = document.getElementById('gridContainer');
+
+
+items.forEach(item => {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridItem.textContent = item;
+    gridContainer.appendChild(gridItem);
+});
+*/
