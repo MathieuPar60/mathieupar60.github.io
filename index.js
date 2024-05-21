5302abdc87d595601ce6ff0011b33d9fc000489d
MathieuPar60 <mathieu-1.parenteau@polymtl.ca>
2024-05-21 14:19:48 -0400

    mp | project added

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-/*const items = ["HTML", "CSS", "JavaScript", "Python", "C++", "React", "Angular"];
 
-const gridContainer = document.getElementById('gridContainer');
+let currentImageIndex = 0;
+const images = ['assets/home.PNG', 'assets/tutorial.PNG', 'assets/tutorial1.PNG', 'assets/tutorial2.PNG', 'assets/tutorial3.PNG', 'assets/classic1.PNG', 'assets/classic2.PNG'];
 
+function showImage(index) {
+    const imageElement = document.getElementById('carousel-image');
+    imageElement.src = images[index];
+    updateButton();
+}
 
-items.forEach(item => {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.textContent = item;
-    gridContainer.appendChild(gridItem);
-});
-*/
+function prevImage() {
+    if (currentImageIndex > 0) {
+        currentImageIndex--;
+        showImage(currentImageIndex);
+    }
+}
+
+function nextImage() {
+    if (currentImageIndex < images.length - 1) {
+        currentImageIndex++;
+        showImage(currentImageIndex);
+    }
+}
+
+function updateButtons() {
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+
+    prevButton.disabled = currentImageIndex === 0;
+    nextButton.disabled = currentImageIndex === images.length - 1;
+}
+
+updateButtons();
